ab817f21d33e8c51d866e5eff57db1ef6bc07d1e
Thomas Duckworth <tduck973564@gmail.com>
2024-06-23 21:29:53 +1000

    Default desktop-layout

--- filotimo-kde-overrides/org.kde.plasma.desktop-layout.js
+++ filotimo-kde-overrides/org.kde.plasma.desktop-layout.js
@@ -1,10 +1,5 @@
 loadTemplate("org.kde.plasma.desktop.defaultPanel")
 
-var desktopsArray = desktopsForActivity(currentActivity());
-for( var j = 0; j < desktopsArray.length; j++) {
-    desktopsArray[j].wallpaperPlugin = 'org.kde.image';
-}
-
 const panel = panels()[0];
 panel.height = Math.round(gridUnit * 3);
 panel.floating = false;
